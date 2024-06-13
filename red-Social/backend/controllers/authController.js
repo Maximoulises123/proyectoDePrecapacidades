a31b5e1a99e793d6bffd70068f3f0f27d75dc827
MaximoU <146015634+Maximoulises123@users.noreply.github.com>
2024-06-13 16:10:28 -0300

    Update authController.js

--- red-Social/backend/controllers/authController.js
+++ red-Social/backend/controllers/authController.js
@@ -1,1 +1,34 @@
+const User = require('../models/User');
 
+const authController = {
+  register: async (req, res) => {
+    const { ID,userName, gmail, contrasenia } = req.body;
+    try {
+      const userExists = await User.buscarEmail(gmail);
+      if (userExists) {
+        return res.status(400).json({ message: 'El usuario ya existe' });
+      }
+      const userId = await User.registrarUsuario(ID, nombreDeUsuario, gmail, contraseña);
+      res.status(201).json({ userId });
+    } catch (error) {
+      res.status(500).json({ message: 'Error en el servidor' });
+    }
+  },
+
+  login: async (req, res) => {
+    const { gmail, contraseña } = req.body;
+    try {
+      const user = await User.buscarEmail(gmail);
+      if (!user || user.contraseña !== contraseña) {
+        return res.status(401).json({ message: 'Credenciales inválidas' });
+      }
+      res.status(200).json({ message: 'Inicio de sesión exitoso' });
+    } catch (error) {
+      res.status(500).json({ message: 'Error en el servidor' });
+    }
+  }
+};
+
+module.exports = authController;
+
+  
