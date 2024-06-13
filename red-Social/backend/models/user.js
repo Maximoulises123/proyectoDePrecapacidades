7e408736e7f0d47caff8971b0bd87a3b2278321f
MaximoU <146015634+Maximoulises123@users.noreply.github.com>
2024-06-13 14:08:11 -0300

    Update user.js

--- red-Social/backend/models/user.js
+++ red-Social/backend/models/user.js
@@ -1,1 +1,21 @@
+const dedsec = require('../config/db.js');
+
+const user = {
+  // Método para buscar un usuario por su correo electrónico
+  buscarEmail: async (gmail) => {
+    const [resultado] = await redsocial.execute('SELECT * FROM users WHERE gmail = ?', [gmail]);
+    return resultado;
+  },
+
+  // Método para registrar un nuevo usuario
+  registrarUsuario: async (id,userName,gmail,contrasenia) => {
+    const [registro] = await dedsec.execute(
+      'INSERT INTO user (userName,gmail,contrasenia) VALUES (?, ?, ?)',
+      [username, gmail, contrasenia]
+    );
+    return registro;
+  }
+};
+
+module.exports = usuario;
 
