dc1fe7843d9ad1c8b047176c46b5da3acd19f2c6
MaximoU <146015634+Maximoulises123@users.noreply.github.com>
2024-06-13 13:45:02 -0300

    Update db.js

--- red-Social/backend/config/db.js
+++ red-Social/backend/config/db.js
@@ -1,1 +1,16 @@
+let mysql= require("mysql");
+let conexion=mysql.createConnection({
+    host:"localhost",
+    database:"dedsec",
+    user:"root",
+    password:"",
+});
+
+conexion.connect(funcion(err){
+    if(err){
+        throw err;
+    } else {
+        console.log("exito");
+}
+});
 
