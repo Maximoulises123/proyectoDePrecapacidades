e7e1cdfa7116c3309a49475171c5cf0745c647d8
MaximoU <146015634+Maximoulises123@users.noreply.github.com>
2024-07-04 10:41:42 -0300

    Update auth.js

--- red-Social/backend/routes/auth.js
+++ red-Social/backend/routes/auth.js
@@ -1,1 +1,17 @@
+const express = require('express'); // Importamos Express para manejar rutas
+const authController = require('../controllers/authController'); // Importamos el controlador que maneja la lógica de autenticación
+const router = express.Router(); // Creamos un enrutador para definir nuestras rutas
+
+// Rutas relacionadas con la autenticación
+
+// Ruta para el registro de usuarios
+router.post('/register', authController.register);
+
+// Ruta para el inicio de sesión de usuarios
+router.post('/login', authController.login);
+
+// Ruta para eliminar cuenta de usuario
+router.delete('/delete', authController.deleteUser);
+
+module.exports = router; // Exportamos el enrutador para que otras partes de la aplicación lo puedan usar
 
